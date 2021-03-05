2d22242aaf229867c9f70030d2fe7c91dc6213ae
Jacob Millner <jacob.millner@outlook.com>
2021-03-04 22:27:08 -0500

    settings page form setup

--- client/screens/settings/index.tsx
+++ client/screens/settings/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, View, Text } from 'react-native';
+import { Input, Select, SelectItem, IndexPath } from '@ui-kitten/components';
 
 interface SettingsProps {
   navigation: any;
@@ -7,11 +8,28 @@ interface SettingsProps {
 
 const SettingsScreen = ({ navigation }: SettingsProps) => {
   const navBack = () => navigation.goBack();
+  const [username, setUserName] = useState<string>();
+  const [selectedTheme, setSelectedTheme] = React.useState<
+    IndexPath | IndexPath[]
+  >();
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Settings Screen</Text>
       <Button title="Back" onPress={navBack} />
+      <Input
+        placeholder="Name"
+        value={username}
+        onChangeText={(nextValue) => setUserName(nextValue)}
+      />
+      <Select
+        label="Theme"
+        caption="Theme"
+        selectedIndex={selectedTheme}
+        onSelect={(index) => setSelectedTheme(index)}>
+        <SelectItem title="Dark" />
+        <SelectItem title="Light" />
+      </Select>
     </View>
   );
 };
